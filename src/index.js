45bbd7905af2ca9c3764db4456d6bb6d6161da0a
Ani <aavetisian@noorlogic.com>
2023-01-23 15:03:22 +0400

    sorted

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return matrix ? matrix.flatMap((m, i) => i % 2 ? m.reverse() : m) : []
 }
+  
+
+// console.log(towelSort( [
+//  [ 1, 2, 3 ],
+//  [ 4, 5, 6 ],
+//  [ 7, 8, 9 ],
+// ]));
